e6e4885a1899d08cf4457b5803340f93d7ec1179
shadow-master-dev <master.blogger.design@gmail.com>
2025-09-04 15:32:23 -0500

    Delete Main/main.js

--- Main/main.js
+++
@@ -1,6 +0,0 @@
-// main.js
-document.addEventListener("DOMContentLoaded", () => {
-  google.script.run.withSuccessHandler(setUserHeader).compararUsuarios();
-  google.script.run.withSuccessHandler(renderMenu).obtenerMenuUsuario();
-  mostrarBreadcrumb();
-});
